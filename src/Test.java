aa2085dbc794e99ec83c4e6b7322d3da087874c0
abdulkerim <abdu0921@hotmail.com>
2023-05-21 16:01:53 -0400

    modified

--- src/Test.java
+++ src/Test.java
@@ -1,27 +1,27 @@
-public class Test {
+//public class Test {
+////    private static String isPalindrome(String str){
+////        String[] strings = str.split(" ");
+////        String largestPalindrome = "";
+////        for (String word : strings) {
+////            String reversedWord = new StringBuilder(word).reverse().toString();
+////            if (word.equalsIgnoreCase(reversedWord) && word.length() > largestPalindrome.length()) {
+////                largestPalindrome = word;
+////            }
+////        }
+////        return largestPalindrome;
+////    }
 //    private static String isPalindrome(String str){
 //        String[] strings = str.split(" ");
 //        String largestPalindrome = "";
-//        for (String word : strings) {
-//            String reversedWord = new StringBuilder(word).reverse().toString();
-//            if (word.equalsIgnoreCase(reversedWord) && word.length() > largestPalindrome.length()) {
-//                largestPalindrome = word;
+//        for (int i = 0; i < strings.length; i++){
+//            String reversed = new StringBuilder(strings[i]).reverse().toString();
+//            if (strings[i].equalsIgnoreCase(reversed) && strings[i].length() > largestPalindrome.length()){
+//                largestPalindrome = strings[i];
 //            }
 //        }
 //        return largestPalindrome;
 //    }
-    private static String isPalindrome(String str){
-        String[] strings = str.split(" ");
-        String largestPalindrome = "";
-        for (int i = 0; i < strings.length; i++){
-            String reversed = new StringBuilder(strings[i]).reverse().toString();
-            if (strings[i].equalsIgnoreCase(reversed) && strings[i].length() > largestPalindrome.length()){
-                largestPalindrome = strings[i];
-            }
-        }
-        return largestPalindrome;
-    }
-    public static void main(String[] args) {
-        System.out.println( isPalindrome("This AZA ASSa is compleTEd"));
-    }
-}
+//    public static void main(String[] args) {
+//        System.out.println( isPalindrome("This AZA ASSa is compleTEd"));
+//    }
+//}
