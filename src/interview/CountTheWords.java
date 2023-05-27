8d9877b74d57047949c1e08f82d5c99540408da5
abdulkerim <abdu0921@hotmail.com>
2023-05-27 14:51:33 -0400

    3rd week

--- src/interview/CountTheWords.java
+++ src/interview/CountTheWords.java
@@ -9,7 +9,7 @@ public class CountTheWords {
         String s = sc.nextLine();
         int count = 1;
         for (int i = 0; i < s.length()-1; i++){
-            if ((s.charAt(i) == ' ') && (s.charAt(i+1) != ' ')) {
+            if ((s.charAt(i) == ' ') && (s.charAt(i+1) != ' ')){
                 count++;
             }
         }
--- src/interview/Test.java
+++ src/interview/Test.java
@@ -2,14 +2,13 @@ package interview;
 
 public class Test {
     public static void main(String[] args) {
-        //factorial
-        int num = 5; //5*4*3*2*1
-        int factorial = 1;
-        int i = 1;
-       while (i <= num){
-           factorial = factorial * i;
-           i++;
-       }
-        System.out.println(factorial);
+        String str =  "my name is abdu";
+        int count = 1;
+        for (int i = 0; i < str.length()-1; i++){
+            if ((str.charAt(i)== ' ') && (str.charAt(i + 1) != ' ')){
+                count++;
+            }
+        }
+        System.out.println("Number of Words in string is " + count);
     }
 }
