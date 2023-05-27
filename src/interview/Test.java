39ff4b41f56a5a85850ada638eba6355ca4081b4
abdulkerim <abdu0921@hotmail.com>
2023-05-27 15:03:35 -0400

    3rd week

--- src/interview/Test.java
+++ src/interview/Test.java
@@ -1,14 +1,35 @@
 package interview;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Test {
     public static void main(String[] args) {
-        String str =  "my name is abdu";
-        int count = 1;
-        for (int i = 0; i < str.length()-1; i++){
-            if ((str.charAt(i)== ' ') && (str.charAt(i + 1) != ' ')){
-                count++;
+        String[] arr = {"java", "c", "c++", "c", "python", "java"};
+//        boolean flag = false;
+//        for (int i = 0; i < arr.length; i++){
+//            for (int j = i + 1; j < arr.length; j++){
+//                if (arr[i] == arr[j]){
+//                    System.out.println("duplicate is found at index " + i);
+//                    flag = true;
+//                }
+//            }
+//        }
+//        if (!flag){
+//            System.out.println("duplicate is not found");
+//        }
+        Set<String> stringSet = new HashSet<>();
+        boolean flag = false;
+        int index = 0;
+        for (String str : arr){
+            if (stringSet.add(str) == false){
+                System.out.println("duplicate is found at index " + index);
+                index++;
+                flag = true;
             }
         }
-        System.out.println("Number of Words in string is " + count);
+        if (!flag){
+            System.out.println("duplicate is not found");
+        }
     }
 }
