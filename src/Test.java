791c3e313ad0a9a378f1f009f3cdf74d3cc80f91
abdulkerim <abdu0921@hotmail.com>
2024-05-13 23:50:09 -0400

    3rd week

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,16 @@
-public class Test {
-    public static void main(String[] args) {
+public class Test implements AutoCloseable{
+    public static void main(String[] args) throws Exception {
+        try (Test test = new Test()){
+            System.out.println(3/0);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println("this is the second one");
+    }
 
+    @Override
+    public void close() throws Exception {
+        System.out.println("the method is closed");
     }
 }
+
--- src/exception/Main.java
+++ src/exception/Main.java
@@ -1,7 +1,39 @@
 package exception;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        A a = new A();
+        B b = new B();
+        a.start();
+        a.join();
+        b.start();
+    }
+}
 
+class A extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+class B extends Thread {
+    @Override
+    public void run() {
+        for (int i = 5; i > 0; i--) {
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
